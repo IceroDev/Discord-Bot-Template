3c00169a38095523128eadbc8a6263bd32522c85
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2020-10-18 10:13:06 +0200

    changement du cache

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,3 @@
 module.exports = (client) => {
-  console.log(`Je suis connecté et prêt à servir `+client.guilds.size +" serveurs et "+client.users.size+" utilisateurs");
+  console.log(`Je suis connecté et prêt à servir `+client.guilds.cache.size +" serveurs et "+client.users.cache.size+" utilisateurs");
 }
